0f3bce72037e8f5eaa8ba382f1b07f04d19fa85e
Tahiryar <tahiryar.251k@gmail.com>
2025-04-15 13:29:49 +0500

    create RequestState class for data format & validation

---
+++ backend.py
@@ -0,0 +1,11 @@
+#Step1: Setup Pydantic Model (Schema Validation)
+from pydantic import BaseModel
+from typing import List
+
+
+class RequestState(BaseModel):
+    model_name: str
+    model_provider: str
+    system_prompt: str
+    messages: List[str]
+    allow_search: bool
