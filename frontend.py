2e350c5f75ca2b767500feac95457773bb79439a
Tahiryar <tahiryar.251k@gmail.com>
2025-04-18 18:52:36 +0500

    Setup groq or openai models

--- frontend.py
+++ frontend.py
@@ -4,3 +4,17 @@ import streamlit as st
 st.set_page_config(page_title="LangGraph Agent UI", layout="centered")
 st.title("AI Chatbot Agents")
 st.write("Create and Interact with the AI Agents!")
+
+system_prompt=st.text_area("Define your AI Agent: ", height=70, placeholder="Type your system prompt here...")
+
+
+MODEL_NAMES_GROQ = ["llama-3.3-70b-versatile", "mixtral-8x7b-32768"]
+MODEL_NAMES_OPENAI = ["gpt-4o-mini"]
+
+provider=st.radio("Select Provider:", ("Groq", "OpenAI"))
+if provider == "Groq":
+    selected_model = st.selectbox("Select Groq Model:", MODEL_NAMES_GROQ)
+elif provider == "OpenAI":
+    selected_model = st.selectbox("Select OpenAI Model:", MODEL_NAMES_OPENAI)
+
+allow_web_search=st.checkbox("Allow Web Search")
