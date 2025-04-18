02934735526a425da18041bd32cd7035d06422ae
Tahiryar <tahiryar.251k@gmail.com>
2025-04-18 18:44:04 +0500

    Setup UI with streamlit

---
+++ frontend.py
@@ -0,0 +1,6 @@
+#Step1: Setup UI with streamlit (model provider, model, system prompt, web_search, query)
+import streamlit as st
+
+st.set_page_config(page_title="LangGraph Agent UI", layout="centered")
+st.title("AI Chatbot Agents")
+st.write("Create and Interact with the AI Agents!")
